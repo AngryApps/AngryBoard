ee6394de6daf41235d506fb6f5081235756689c0
luizgpp <luizgpp@gmail.com>
2025-01-24 02:32:30 -0300

    feat(board): add e2e test command and update config
    - Added "e2e:dev" script to package.json for running Playwright tests
    - Updated playwright.config.ts to include testMatch pattern
    - Changed baseURL and webServer URL to 'http://localhost:4200'

--- board/playwright.config.ts
+++ board/playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
+  testMatch: '**/*.spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'localhost:4000',
+    baseURL: 'http://localhost:4200/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'localhost:4000',
+    url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
   },
 });
