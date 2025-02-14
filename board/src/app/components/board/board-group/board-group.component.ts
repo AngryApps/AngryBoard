5108e8bd7b731e146f27cdddfeb3417f2cb442fd
Luiz Gabriel Parreira Pereira <luizgpp@gmail.com>
2025-02-14 01:25:10 -0300

    Feat/edit delete column (#9)
    * feat(column): implement delete column functionality and improve action menu
    
    - Added `deleteColumn` method to `ColumnService` for removing columns.
    - Integrated confirmation dialog in `ActionMenuComponent` for delete action.
    - Updated `BoardColumnComponent` to handle column deletion.
    - Minor adjustments in action menu and column component templates for better UI.
    
    * feat(tests): add HttpClient provider to BoardColumnComponent tests
    
    - Included `provideHttpClient` in the test setup for the BoardColumnComponent.
    - This change allows the tests to properly handle HTTP requests and responses.
    
    * feat: make components standalone for improved modularity
    
    - Updated AppComponent, CardBodyComponent, ActionMenuComponent,
      BoardColumnComponent, BoardGroupComponent, and AddColumnComponent
      to be standalone.
    - Adjusted template formatting for AppComponent to improve readability.
    - Imported CommonModule in BoardColumnComponent for better
      functionality.
    
    * feat(board): add edit column functionality and improve error handling
    
    - Introduced `editColumn` method in `column.service.ts` to
      update existing columns with new titles and descriptions.
    - Updated error handling to specify error type as `string` in
      relevant methods.
    - Integrated `InplaceInputComponent` for inline editing of
      column titles and descriptions in `board-column.component.html`.
    - Added `UpdateColumnRequest` interface to define the structure
      for editing a column.
    - Enhanced `board-column.component.ts` to handle title and
      description edits using the newly created service method.
    - Added export for `InplaceInputComponent` in shared components.
    - Miscellaneous improvements to code formatting and structure.

--- board/src/app/components/board/board-group/board-group.component.ts
+++ board/src/app/components/board/board-group/board-group.component.ts
@@ -28,9 +28,10 @@ import { AddColumnComponent } from '../add-column/add-column.component';
   templateUrl: './board-group.component.html',
   styleUrl: './board-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
 })
 export class BoardGroupComponent implements OnInit {
-  columnService: ColumnService = inject(ColumnService);
+  columnService = inject(ColumnService);
 
   columns$ = this.columnService.columns;
 
---
+++ board/src/app/shared/components/inplace-input/inplace-input.component.ts
@@ -0,0 +1,70 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  model,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { Inplace, InplaceModule } from 'primeng/inplace';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonGroupModule } from 'primeng/buttongroup';
+import { CheckIcon, TimesIcon } from 'primeng/icons';
+import { AutoFocusModule } from 'primeng/autofocus';
+
+@Component({
+  selector: 'inplace-input',
+  imports: [
+    InplaceModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    ButtonGroupModule,
+    CheckIcon,
+    TimesIcon,
+    AutoFocusModule,
+  ],
+  templateUrl: './inplace-input.component.html',
+  styleUrl: './inplace-input.component.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InplaceInputComponent {
+  inplaceRef = viewChild.required<Inplace>(Inplace);
+
+  value = model<string>('');
+  placeholder = input<string>('');
+  actionButtons = input<boolean>(false);
+  saved = output<string>();
+  closed = output<void>();
+  tempValue = signal('');
+  displayValue = computed(() => this.value() || this.placeholder());
+
+  onActivate(): void {
+    this.tempValue.set(this.value());
+  }
+
+  onDeactivate(): void {
+    // Clean up if needed
+  }
+
+  updateTempValue(newValue: string): void {
+    this.tempValue.set(newValue);
+  }
+
+  save(): void {
+    this.value.set(this.tempValue());
+    this.saved.emit(this.tempValue());
+    this.inplaceRef().deactivate();
+  }
+
+  close(): void {
+    this.tempValue.set(this.value());
+    this.inplaceRef().deactivate();
+    this.closed.emit();
+  }
+}
