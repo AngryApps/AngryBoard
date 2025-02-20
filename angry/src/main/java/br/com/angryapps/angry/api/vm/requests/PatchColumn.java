20c747b13b06e250be644a44b39021cffb73dbf7
Thiago Miranda <marine.rlivielor@gmail.com>
2025-02-19 23:46:21 -0300

    Adding patch route for column and fixing updates from PUT method

---
+++ angry/src/main/java/br/com/angryapps/angry/api/vm/requests/PatchColumn.java
@@ -0,0 +1,32 @@
+package br.com.angryapps.angry.api.vm.requests;
+
+public class PatchColumn {
+
+    private String title;
+    private String description;
+    private Integer position;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
